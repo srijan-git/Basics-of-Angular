c51da8525a4e1b0c92c52973a1801a9e25675bb9
Srijan Khan <srijan.khan20@gmail.com>
2023-06-26 12:40:26 +0530

    Code added

--- src/app/Components/observable-httpclient-rx-js/observable-httpclient-rx-js.component.ts
+++ src/app/Components/observable-httpclient-rx-js/observable-httpclient-rx-js.component.ts
@@ -1,24 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-observable-httpclient-rx-js',
   templateUrl: './observable-httpclient-rx-js.component.html',
   styleUrls: ['./observable-httpclient-rx-js.component.css'],
 })
-export class ObservableHTTPCLientRxJSComponent implements OnInit {
-  myPromise = new Promise((resolve, reject) => {
-    console.log('Promise Started');
-  });
-  myObservable = new Observable((observer) => {
-    console.log('Observable Started');
-    observer.next('A');
-    observer.next('B');
-    observer.next('C');
-  });
+export class ObservableHTTPCLientRxJSComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
   ngOnInit(): void {
-    // this.myObservable.subscribe((res) => {
+    const myPromise = new Promise((resolve, reject) => {
+      console.log('Promise Call.....');
+      resolve('Promise Started');
+      resolve('Promise Started');
+    });
+
+    const myObservable = new Observable((observer) => {
+      console.log('Observable started');
+      let count = 0;
+      setInterval(() => {
+        count = count + 1;
+        observer.next(count);
+      }, 1000);
+    });
+
+    // this.subscription = myObservable.subscribe((res) => {
     //   console.log(res);
     // });
+
+    // Observables are unicast
+    // const myObservable2 = new Observable((observer) => {
+    //   observer.next(Math.random());
+    // });
+
+    // Subscriber 1
+    // myObservable2.subscribe((res) => console.log(res));
+
+    // Subscriber 2
+    // myObservable2.subscribe((res) => console.log(res));
+
+    // Observables are unicast
+    // const mySubject = new Subject();
+    // Subscriber 1
+    // mySubject.subscribe((res) => console.log(res));
+    // Subscriber 2
+    // mySubject.subscribe((res) => console.log(res));
+    // mySubject.next(Math.random());
+
+    // const data = ajax('https://jsonplaceholder.typicode.com/users');
+    // console.log(data);
+    // const subject = new Subject();
+    // subject.subscribe((d) => console.log(d));
+    // subject.subscribe((d) => console.log(d));
+    // subject.subscribe((d) => console.log(d));
+    // data.subscribe(subject);
+
+
+    
+  }
+  ngOnDestroy(): void {
+    // this.subscription.unsubscribe();
   }
 }
